0d9a5cd98b4ac2e8d0d4947f293cf682d6a57a50
eleoes <leo.escaroz@gmail.com>
2023-01-10 20:10:16 -0800

    wip: header comp, mobile

---
+++ src/scripts/menu.js
@@ -0,0 +1,7 @@
+document.querySelector('.nav__toggle').addEventListener('click', () => {
+    document.querySelector('.nav__menu').classList.toggle('show-menu');
+});
+
+document.querySelector('.nav__close').addEventListener('click', () => {
+    document.querySelector('.nav__menu').classList.toggle('show-menu');
+});
